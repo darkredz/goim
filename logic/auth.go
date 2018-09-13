6ae0eafaa862918ee0d09c5f5eb7dcbfaa143f55
kevinlimtamgo <kevinlim@tamago.live>
2018-09-13 15:57:37 +0800

    simple implementation of auth

--- logic/auth.go
+++ logic/auth.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-	"goim/libs/define"
-	"strconv"
+    "goim/libs/define"
+    "encoding/json"
 )
+
+type TmgToken struct {
+    Uid int64 `json:"Uid"`
+    Rid int32 `json:"Rid"`
+}
 
 // developer could implement "Auth" interface for decide how get userId, or roomId
 type Auther interface {
-	Auth(token string) (userId int64, roomId int32)
+    Auth(token string) (userId int64, roomId int32)
 }
 
 type DefaultAuther struct {
 }
 
 func NewDefaultAuther() *DefaultAuther {
-	return &DefaultAuther{}
+    return &DefaultAuther{}
 }
 
 func (a *DefaultAuther) Auth(token string) (userId int64, roomId int32) {
-	var err error
-	if userId, err = strconv.ParseInt(token, 10, 64); err != nil {
-		userId = 0
-		roomId = define.NoRoom
-	} else {
-		roomId = 1 // only for debug
-	}
-	return
+    var tmgtkn TmgToken
+    err := json.Unmarshal([]byte(token), &tmgtkn)
+    if err != nil {
+        userId = 0
+        roomId = define.NoRoom
+    } else {
+        userId = tmgtkn.Uid
+        roomId = tmgtkn.Rid
+    }
+    return
 }
